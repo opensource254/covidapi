d50137bdf9a29f9643287d800d8b476c346e6730
Stanley Masinde <stanleyloren@gmail.com>
2020-04-12 18:43:03 +0300

    Tests: 	- Fixed the tests and added a basic tes
    Signed-off-by: Stanley Masinde <stanleyloren@gmail.com>

---
+++ test.js
@@ -0,0 +1,15 @@
+var assert = require("assert");
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("./index");
+let should = chai.should();
+chai.use(chaiHttp);
+
+describe('APPLICATION REQUESTS', () => {
+    it('The welcome page SHOULD respond with 200', (done) => {
+        chai.request(server).get('/').end((err, result) => {
+            result.should.have.status(200)
+            done(err)
+        })
+    })
+})
