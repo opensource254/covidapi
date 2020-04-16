c1ceee70e7f87489b6c36d9570bf19ae4ba2f429
Wilcox <rongunowilcox@gmail.com>
2020-04-16 21:54:02 +1000

    Create routes

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
+
 const app = express();
-const ApiV1 = require('./api/v1/routes/routes')
+
 const port = process.env.PORT || 5000;
+
+// routes for the app
+const ApiV1 = require('./api/v1/routes/routes');
+const usersRoute = require('./api/v1/routes/users');
+const DoctorsRoute = require('./api/v1/routes/doctors');
+const adminRoute = require('./api/v1/routes/admin');
+const indexRoute = require('./api/v1/routes/index');
 
 // inject middleware into the app
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/', (req, res) => res.json({ message: 'covidapi is working!!!' }));
-app.use('/api/v1', ApiV1)
+
+app.use('/', indexRoute);
+app.use('/api/v1', ApiV1);
+app.use('/admin', adminRoute);
+app.use('/users', usersRoute);
+app.use('/doctors', DoctorsRoute);
 
 // fire up the application
 app.listen(port, () => console.log(`app is listening on ${port}`));
 
-module.exports = app
+module.exports = app;
