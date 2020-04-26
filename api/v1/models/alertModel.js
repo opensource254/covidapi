9ee738c754d6f39178fa10be68c88775414554f6
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-04-26 19:45:42 +0300

    create alerts model

---
+++ api/v1/models/alertModel.js
@@ -0,0 +1,24 @@
+const db = require('../db/db');
+const Sequelize = require('../db/index');
+
+const Alert = db.define('alerts', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    time: {
+        type: Sequelize.DATE,
+        allowNull: false,
+    },
+    detail: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+});
+
+Alert.sync()
+    .then(() => console.log('Alert table created succesfully'))
+    // eslint-disable-next-line no-unused-vars
+    .catch((err) => console.log('Unable to create the user table'));
+
+module.exports = Alert;
