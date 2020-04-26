9212fa4f9057fa387705b8e51e9fd5f80ef3fe25
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-04-26 15:15:13 +0300

    refactor app entry point

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
 require('dotenv').config();
 const express = require('express');
-const db = require('./api/v1/db/db');
+const routes = require('./api/v1/routes/routes');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// routes for the app
-const ApiV1 = require('./api/v1/routes/routes');
-const usersRoute = require('./api/v1/routes/usersRouter');
-const DoctorsRoute = require('./api/v1/routes/doctorsRouter');
-const adminRoute = require('./api/v1/routes/adminRouter');
-const homeRouter = require('./api/v1/routes/homeRouter');
-
-app.use('/', homeRouter);
-app.use('/api/v1', ApiV1);
-app.use('/admin', adminRoute);
-app.use('/users', usersRoute);
-app.use('/doctors', DoctorsRoute);
+app.use(routes);
 
 module.exports = app;
