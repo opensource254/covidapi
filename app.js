4568eb5da48a10684b03f25e06c613b2cabbe298
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-04-26 12:20:26 +0300

    reorder the app.js and remove knex

--- app.js
+++ app.js
@@ -1,30 +1,18 @@
 require('dotenv').config();
 const express = require('express');
+const db = require('./api/v1/db/db');
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // routes for the app
-const db = require('knex')({
-    client: 'pg',
-    connection: process.env.PG_CONNECTION_STRING,
-    searchPath: ['knex', 'public'],
-    pool: { min: 0, max: 7 },
-});
-
 const ApiV1 = require('./api/v1/routes/routes');
 const usersRoute = require('./api/v1/routes/usersRouter');
 const DoctorsRoute = require('./api/v1/routes/doctorsRouter');
 const adminRoute = require('./api/v1/routes/adminRouter');
 const homeRouter = require('./api/v1/routes/homeRouter');
 
-// inject middleware into the app
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// knex orm
-
-// use knex as db all over
-app.set('db', db);
 app.use('/', homeRouter);
 app.use('/api/v1', ApiV1);
 app.use('/admin', adminRoute);
