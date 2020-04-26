6b6537101d11bb2194adbd78fd398c959f6c0650
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-04-26 15:14:45 +0300

    delete router files

--- api/v1/routes/homeRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express');
-
-const homeRouter = Router();
-
-homeRouter.get('/', (req, res) => {
-    res.json('Home route Working 🚀');
-});
-
-module.exports = homeRouter;
--- api/v1/routes/usersRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express');
-
-const userRoute = Router();
-
-userRoute.get('/', (req, res) => {
-    res.json('Users route is  Working 🚀');
-});
-
-module.exports = userRoute;
