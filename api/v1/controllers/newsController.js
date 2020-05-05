f1913f79b545245ae3a6ddf2f5afc8a272f34032
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-05-05 20:53:18 +0300

    remove savetweets function

--- api/v1/controllers/newsController.js
+++ api/v1/controllers/newsController.js
@@ -1,30 +1,5 @@
-const axios = require('axios');
 require('../db/mongodb');
 const NewsModel = require('../models/newsModel');
-
-function saveTweets() {
-    axios
-        .get('http://localhost:1234/small.json')
-        .then((res) =>
-            res.data.map((obj) => {
-                const news = new NewsModel({
-                    text: obj.text,
-                    timestamp: new Date(obj.timestamp).toLocaleDateString('en-KE', {
-                        day: 'numeric',
-                        month: 'short',
-                        year: 'numeric',
-                        hour: '2-digit',
-                        minute: '2-digit',
-                    }),
-                    username: obj.username,
-                    screen_name: obj.screen_name,
-                    img_urls: obj.img_urls,
-                });
-                news.save();
-            })
-        )
-        .catch((err) => console.log(err));
-}
 
 const News = {
     async getTweets(req, res) {
@@ -38,7 +13,5 @@ const News = {
         }
     },
 };
-
-// saveTweets();
 
 module.exports = News;
