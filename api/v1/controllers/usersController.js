947a1b22373deb57a05234bcd9990be28a57e5c9
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-05-08 16:26:06 +0300

    create the signup method for the user

---
+++ api/v1/controllers/usersController.js
@@ -0,0 +1,25 @@
+﻿const Role = require('../helpers/role');
+const userService = require('./user.service');
+const User = require('../models/0-usersModel');
+
+const userMethods = {
+    async signup(req, res) {
+        try {
+            const { firstname, lastname, email, password, location } = req.body;
+            const user = await User.create({ firstname, lastname, email, password, location });
+            user.save()
+                .then((userparam) => {
+                    return res.json({ status: 201, data: userparam });
+                })
+                .catch((err) => {
+                    console.log('Couldnt create a user');
+                    res.json(err);
+                });
+        } catch (error) {
+            console.error(error);
+            res.json(error);
+        }
+    },
+};
+
+module.exports = userMethods;
