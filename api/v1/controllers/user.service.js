c920a2ed11e0a86cd04920eb3d0e57671c8ab1e4
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-05-09 17:15:23 +0300

    removed some methods

--- api/v1/controllers/user.service.js
+++ api/v1/controllers/user.service.js
@@ -1,38 +1,9 @@
 ﻿/* eslint-disable no-shadow */
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
-const Role = require('../helpers/role');
-
-// users hardcoded for simplicity, store in a db for production applications
-const users = [
-    {
-        id: 1,
-        username: 'admin',
-        password: 'admin',
-        firstName: 'Admin',
-        lastName: 'User',
-        role: Role.Admin,
-    },
-    {
-        id: 2,
-        username: 'user',
-        password: 'user',
-        firstName: 'Normal',
-        lastName: 'User',
-        role: Role.User,
-    },
-];
 
 async function authenticate({ username, password }) {
-    const user = await users.find((u) => u.username === username && u.password === password);
-    if (user) {
-        const token = jwt.sign({ sub: user.id, role: user.role }, process.env.SECRET);
-        const { password, ...userWithoutPassword } = user;
-        return {
-            ...userWithoutPassword,
-            token,
-        };
-    }
+
 }
 
 async function getAll() {
