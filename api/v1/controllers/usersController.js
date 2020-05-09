d158c0cd38e7521dfc9ce73c242e922941741f54
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-05-09 17:17:14 +0300

    add a method to generate a token on login

--- api/v1/controllers/usersController.js
+++ api/v1/controllers/usersController.js
@@ -9,10 +9,11 @@ const userMethods = {
     async signup(req, res) {
         try {
             const { firstname, lastname, email, password, location } = req.body;
+            // const token = await User.generateToken(req.body.email);
             const user = await User.create({ firstname, lastname, email, password, location });
             user.save()
                 .then((userparam) => {
-                    return res.status(201).json({ status: 201, data: userparam });
+                    return res.status(201).json({ userparam });
                 })
                 .catch((err) => {
                     console.log('Couldnt create a user');
@@ -26,7 +27,7 @@ const userMethods = {
     async login(req, res) {
         try {
             const user = await User.checkCredentials(req.body.email, req.body.password);
-            const token = jwt.sign({ sub: User.id, role: User.role }, process.env.SECRET);
+            const token = await User.generateToken(req.body.email); // jwt.sign({ id: User.id, role: User.role }, process.env.SECRET);
             if (user) {
                 return res.status(200).json({
                     status: 200,
