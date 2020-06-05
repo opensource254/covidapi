628a0568388015e38ddbdbfcdb393e652c256f88
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-06-05 15:58:13 +0300

    TODO: will fix this mess later

--- api/v1/controllers/newsController.js
+++ api/v1/controllers/newsController.js
@@ -1,15 +1,42 @@
+/* eslint-disable no-sequences */
+/* eslint-disable camelcase */
 require('../db/mongodb');
+const axios = require('axios');
 const NewsModel = require('../models/newsModel');
+
+class Tweets {
+    constructor(text, timestamp, timestamp_relative, username, screen_name, img_urls) {
+        this.text = text,
+            this.timestamp = timestamp,
+            this.timestamp_relative = timestamp_relative,
+            this.username = username,
+            this.screen_name = screen_name,
+            this.img_urls = img_urls
+    }
+};
 
 const News = {
     async getTweets(req, res) {
+        const tweets = await axios.get('https://twitter.covid19kenya.site/api/v2/moh_kenya');
+        const allTweets = tweets.data;
         try {
-            const news = await NewsModel.find({});
+            // const news = await NewsModel.find({});
+            const filtered_tweets = allTweets.map((obj) => {
+                const sortedTweets = new Tweets({
+                    text: obj.tweet,
+                    timestamp: obj.created_at,
+                    timestamp_relative: obj.relative_time,
+                    username: obj.user,
+                    screen_name: 'MOH_KENYA',
+                    img_urls: obj.media,
+                });
+            });
+            // console.log(typeof tweets);
             res.json({
-                tweets: news,
+                sortedTweets,
             });
         } catch (error) {
-            res.status(500).send();
+            res.status(500).send(error.message);
         }
     },
 };
