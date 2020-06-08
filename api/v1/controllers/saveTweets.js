e811d89abd43cdfaa0021d896d4130ff64038482
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-06-08 14:51:41 +0300

    add savetweets functionality to use later

--- api/v1/controllers/saveTweets.js
+++ api/v1/controllers/saveTweets.js
@@ -1,11 +1,29 @@
 /* eslint-disable array-callback-return */
 const axios = require('axios');
-require('../db/mongodb');
 const NewsModel = require('../models/newsModel');
 
+// (async function saveTweets(req, res) {
+//     const rawTweets = await axios.get('https://twitter.covid19kenya.site/api/v2/moh_kenya');
+//     const allTweets = rawTweets.data;
+//     try {
+//         const tweets = NewsModel.create({ allTweets });
+//         tweets
+//             .save()
+//             .then((news) => {
+//                 return res.json({ status: 201, data: [news] });
+//             })
+//             .catch((err) => {
+//                 res.status(400).json(err);
+//                 console.log(err);
+//             });
+//     } catch (error) {
+//         res.status(500).json(error);
+//         console.log(error);
+//     }
+// })();
 (function saveTweets() {
     axios
-        .get('http://localhost:1234/moh.json')
+        .get('https://twitter.covid19kenya.site/api/v2/moh_kenya')
         .then((res) =>
             res.data.map((obj) => {
                 const news = new NewsModel({
@@ -17,6 +35,7 @@ const NewsModel = require('../models/newsModel');
                         hour: '2-digit',
                         minute: '2-digit',
                     }),
+                    timestamp_relative: obj.timestamp_relative,
                     username: obj.username,
                     screen_name: obj.screen_name,
                     img_urls: obj.img_urls,
@@ -26,3 +45,26 @@ const NewsModel = require('../models/newsModel');
         )
         .catch((err) => console.log(err));
 })();
+
+
+// async getTweets(req, res) {
+//     try {
+//         await NewsModel.findAll()
+//             .then((news) => {
+//                 if (!news || news === ['']) {
+//                     res.status(404).json('No news found');
+//                 }
+//                 res.status(200).json({
+//                     news,
+//                 });
+//             })
+//             .catch(function (err) {
+//                 res.status(400).json({
+//                     err,
+//                 });
+//             });
+//     } catch (error) {
+//         res.status(500).json(error);
+//         console.log(error);
+//     }
+// },
