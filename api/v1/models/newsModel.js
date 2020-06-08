5074a602e86bb481d444b092b36ba056697c2d44
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-06-08 14:51:12 +0300

    change to postgresql model

--- api/v1/models/newsModel.js
+++ api/v1/models/newsModel.js
@@ -1,33 +1,39 @@
-const mongoose = require('mongoose');
+const colors = require('colors');
+const db = require('../db/db');
+const Sequelize = require('../db/index');
 
-const NewsSchema = new mongoose.Schema({
-    text: {
-        type: String,
-        trim: true,
-        required: true,
+const News = db.define(
+    'news',
+    {
+        text: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        timestamp: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        timestamp_relative: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        username: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        screen_name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            defaultValue: 'MOH_KENYA',
+        },
     },
-    timestamp: {
-        type: String,
-        default: 0,
-        required: true,
-    },
-    username: {
-        type: String,
-        trim: true,
-        required: true,
-    },
-    screen_name: {
-        type: String,
-        trim: true,
-        required: true,
-    },
-    img_urls: {
-        type: Array,
-        trim: true,
-        required: true,
-    },
-});
+    {
+        freezeTableName: true,
+    }
+);
 
-const News = mongoose.model('News', NewsSchema);
+News.sync({ alter: true })
+    .then(() => console.log(colors.green('News table created succesfully')))
+    .catch((err) => console.log(colors.red('Unable to create the news table', err)));
 
 module.exports = News;
