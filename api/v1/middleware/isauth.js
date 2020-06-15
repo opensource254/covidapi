b00c80f807f1b9f0f420fe4024bbf1da10258424
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-06-15 13:08:32 +0300

    delete isauth file due to redundancy

--- api/v1/middleware/isauth.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    IsLoggedin: (req, res, next) => {
-        if (req.session.isLoggedin) {
-            return next();
-        }
-        res.status(401).json({ message: 'You are not logged in' });
-    },
-};
