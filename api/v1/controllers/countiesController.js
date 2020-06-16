0d290e17f12ad08d019e7db53cfbb9fe96adf983
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-06-16 13:10:01 +0300

    remove repeated code to make use of the crud utility functions

--- api/v1/controllers/countiesController.js
+++ api/v1/controllers/countiesController.js
@@ -1,5 +1,5 @@
 const County = require('../models/countyModel');
-const { Sequelize } = require('../db');
+const { utilGetAll, utilCreate, utilGetOne, utilDelete } = require('../helpers/utilService');
 
 const countyController = {
     async create(req, res) {
@@ -9,79 +9,33 @@ const countyController = {
             type: 'Point',
             coordinates: [req.body.lat, req.body.lon],
         };
-
-        const con = await County.create({
-            county_id,
-            county,
-            cases,
-            recoveries,
-            tests,
-            deaths,
-            position: point,
-        });
-        con.save()
-            .then((_county) => {
-                return res.status(201).json({ data: [_county] });
-            })
-            .catch(Sequelize.ValidationError, (err) => {
-                console.log('Couldnt create a county case');
-                res.status(422).json(err);
+        try {
+            await utilCreate(req, res, County, {
+                county_id,
+                county,
+                cases,
+                recoveries,
+                tests,
+                deaths,
+                position: point,
             });
+        } catch (error) {
+            res.status(422).json(error.message);
+        }
     },
     async getAll(req, res) {
-        await County.findAll()
-            .then((county) => {
-                if (!county) {
-                    res.status(404).json('No conty cases found');
-                }
-                res.status(200).json({
-                    data: county,
-                });
-            })
-            .catch(function (err) {
-                res.status(422).json({
-                    err,
-                });
-            });
+        await utilGetAll(req, res, County);
     },
     async getOne(req, res) {
-        const _id = req.params.id;
-        await County.findOne({
-            where: {
-                id: _id,
-            },
-        })
-            .then((con) => {
-                if (!con) {
-                    res.status(404).json('No county cases was found');
-                } else {
-                    console.log(`retrived county case ${JSON.stringify(con, null, 2)}`);
-                    res.status(200).json(con);
-                }
-            })
-            .catch((err) => {
-                console.log(err);
-                res.status(422).json('No coounty cases was found');
-            });
+        const { id } = req.params;
+        await utilGetOne(req, res, County, id);
     },
     async deleteCounty(req, res) {
         const { id } = req.params;
         try {
-            County.findOne({ where: { id } }).then((county) => {
-                if (!county) {
-                    res.status(404).json('No County was found');
-                }
-                County.destroy({ where: { id }, returning: true, plain: true })
-                    .then((deletedCounty) => {
-                        res.status(200).json(`Successfully deleted the County with the id  ${id}`);
-                    })
-                    .catch((err) => {
-                        res.status(422).json('Could not delete the County');
-                        console.log(err);
-                    });
-            });
+            utilDelete(req, res, County, id);
         } catch (error) {
-            res.status(500).json(error);
+            res.status(422).json(error);
         }
     },
 };
