e0ea817c0a39fc43f29ae6ccf7246fad1f09288d
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-06-21 10:13:48 +0300

    create geofence controller

---
+++ api/v1/controllers/geoFenceController.js
@@ -0,0 +1,43 @@
+const Geofence = require('../models/geoFenceModel');
+const {
+    utilGetAll,
+    utilCreate,
+    utilGetOne,
+    utilDelete,
+    utilUpdate,
+} = require('../helpers/utilService');
+
+const GeofenceController = {
+    async create(req, res) {
+        const point = {
+            type: 'Point',
+            coordinates: [req.body.lat, req.body.lon],
+        };
+        try {
+            await utilCreate(req, res, Geofence, { position: point });
+        } catch (error) {
+            res.status(422).json(error.message);
+        }
+    },
+    async getAll(req, res) {
+        await utilGetAll(req, res, Geofence);
+    },
+    async getOne(req, res) {
+        const { id } = req.params;
+        await utilGetOne(req, res, Geofence, id);
+    },
+    async update(req, res) {
+        const { id } = req.params;
+        const point = {
+            type: 'Point',
+            coordinates: [req.body.lat, req.body.lon],
+        };
+        await utilUpdate(req, res, Geofence, { position: point }, id);
+    },
+    async delete(req, res) {
+        const { id } = req.params;
+        await utilDelete(req, res, Geofence, id);
+    },
+};
+
+module.exports = GeofenceController;
